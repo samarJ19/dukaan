bb11622c2288a358750505244192b86622d158df
samarJ19 <samarjoshitech@gmail.com>
2024-04-16 19:49:57 +0530

    grid update

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ function App() {
     
     
     
-    <div className="grid grid-cols-[14%86%]"> {/*h-screen sticky and aside tag worked for a sticky side bar */}
+    <div className="grid grid-cols-[20%90%] md:grid-cols-[20%90%] lg:grid-cols-[14%86%]"> {/*h-screen sticky and aside tag worked for a sticky side bar */}
     <aside class="h-screen sticky top-0 overflow-y-screen">
       <Sidebar></Sidebar>
       </aside>  
-    <main>
+    <main >
     <Topbar text1={"Payouts"} text2={"How it works"}></Topbar>
     <Overview text={"Overview"}></Overview>
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
     <RevenueCard2 title2={"Next payout"} orderCount2={"23"}amount2={"2,312.23"}nextdate={""}time={ "04:00 PM"} ></RevenueCard2>
     <RevenueCard className="" title={"Amount Pending"} orderCount={"13"} amount={"92,312.20"}/>
     <RevenueCard3 title={"Amount Processed"} amount={"23,92,312.19"}/>
